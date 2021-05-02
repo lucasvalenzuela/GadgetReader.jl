d36120c40855426dad87c6991ac234190fb09fcf
Lucas Valenzuela <lucasvalenzuela@users.noreply.github.com>
2021-05-02 17:39:51 +0200

    Remove documentation testing with too long outputs

--- src/galaxy.jl
+++ src/galaxy.jl
@@ -11,7 +11,7 @@ The Dict has String keys, which are set to all uppercase by default.
 The properties are accessible via `particles.prop` (case insensitive).
 
 # Examples
-```jldoctest
+```
 julia> p = GadgetGalaxies.Particles(:stars, Dict("ID"=>[1, 2], "MASS"=>[1e11, 2e11]))
 [...]
 
@@ -87,7 +87,7 @@ The particles are accessible via `galaxy.particletype` or `galaxy[:particletype]
 (see [`particles`](@ref) for typical types).
 
 # Examples
-```jldoctest
+```
 julia> p = GadgetGalaxies.Particles(:stars, Dict("ID"=>[1, 2], "MASS"=>[1e11, 2e11]))
 [...]
 
