ec5483a1bfe20411353a056a16845c24beac256e
Lucas Valenzuela <lucasvalenzuela@users.noreply.github.com>
2021-10-25 11:39:31 +0200

    Start replacement with reading units from Dict

--- src/units.jl
+++ src/units.jl
@@ -1,5 +1,5 @@
 """
-    convert_units!(p::Particles, h, units::Symbol=:full)
+    convert_units!(p::Particles, h::AbstractGadgetHeader, units::Symbol=:full)
 
 Converts the units of the particles properties (see [`Particles`](@ref)) from a snapshot or
 subfind header in-place.
@@ -8,7 +8,7 @@ to physical units (kpc, km/s, solar metallicities etc.), or `:sim` for values in
 The individual metallicities in "Zs" are converted to a one-dimensional `Vector` of values in solar
 metallicities in any case.
 """
-function convert_units!(p::Particles, h, units::Symbol=:full)
+function convert_units!(p::Particles, h::AbstractGadgetHeader, units::Symbol=:full)
     if units === :physical
         for (key, vals) in pairs(p)
             convert_units_physical!(vals, key, h)
@@ -27,10 +27,232 @@ function convert_units!(p::Particles, h, units::Symbol=:full)
     return p
 end
 
+function convert_to_unitful!(
+    a::AbstractArray{T},
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+) where {T<:Real}
+    haskey(simunits, prop) || return a
+
+    factor_func, u = simunits[prop]
+
+    if !isnothing(factor_func)
+        a .*= factor_func(h)
+    end
+
+    if !isnothing(u)
+        Q = Quantity{T,dimension(u),typeof(u)}
+        return reinterpret(Q, a)
+    end
+
+    return a
+end
+
+function convert_to_physical!(
+    a::AbstractArray{<:Real},
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    haskey(simunits, prop) || return a
+
+    factor_func, _ = simunits[prop]
+
+    if !isnothing(factor_func)
+        a .*= factor_func(h)
+    end
+
+    return a
+end
+
+function convert_to_sim!(
+    a::AbstractArray{<:Real},
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    haskey(simunits, prop) || return a
+
+    factor_func, _ = simunits[prop]
+
+    if !isnothing(factor_func)
+        a ./= factor_func(h)
+    end
+
+    return a
+end
+
+function convert_to_sim!(
+    a::AbstractArray{Quantity},
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    astrip = ustrip(a)
+    haskey(simunits, prop) || return astrip
+
+    factor_func, _ = simunits[prop]
+
+    if !isnothing(factor_func)
+        astrip ./= factor_func(h)
+    end
+
+    return astrip
+end
+
+
+function convert_to_unitful(
+    a::AbstractArray{T},
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+) where {T<:Real}
+    haskey(simunits, prop) || return copy(a)
+
+    factor_func, u = simunits[prop]
+
+    if isnothing(factor_func)
+        if isnothing(u)
+            return copy(a)
+        else
+            return a .* u
+        end
+    else
+        if isnothing(u)
+            return a .* factor_func(h)
+        else
+            return a .* factor_func(h) .* u
+        end
+    end
+end
+
+function convert_to_physical(
+    a::AbstractArray{<:Real},
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    haskey(simunits, prop) || return copy(a)
+
+    factor_func, _ = simunits[prop]
+
+    if !isnothing(factor_func)
+        return a .* factor_func(h)
+    end
+
+    return copy(a)
+end
+
+function convert_to_sim(
+    a::AbstractArray{<:Real},
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    haskey(simunits, prop) || return copy(a)
+
+    factor_func, _ = simunits[prop]
+
+    if !isnothing(factor_func)
+        return a ./ factor_func(h)
+    end
+
+    return copy(a)
+end
+
+function convert_to_sim(
+    a::AbstractArray{Quantity},
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    astrip = ustrip(a)
+    haskey(simunits, prop) || return copy(astrip)
+
+    factor_func, _ = simunits[prop]
+
+    if !isnothing(factor_func)
+        return astrip ./ factor_func(h)
+    end
+
+    return copy(astrip)
+end
+
+function convert_to_unitful(a::Real, prop::AbstractString, h::AbstractGadgetHeader, simunits::Dict)
+    haskey(simunits, prop) || return a
+
+    factor_func, u = simunits[prop]
+
+    if !isnothing(factor_func)
+        a *= factor_func(h)
+    end
+
+    if isnothing(u)
+        return a
+    else
+        return a * u
+    end
+end
+
+function convert_to_physical(
+    a::Real,
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    haskey(simunits, prop) || return a
+
+    factor_func, _ = simunits[prop]
+
+    if isnothing(factor_func)
+        return a
+    else
+        return a * factor_func(h)
+    end
+end
+
+function convert_to_sim(
+    a::Real,
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    haskey(simunits, prop) || return a
+
+    factor_func, _ = simunits[prop]
+
+    if isnothing(factor_func)
+        return a
+    else
+        return a / factor_func(h)
+    end
+end
+
+function convert_to_sim(
+    a::Quantity,
+    prop::AbstractString,
+    h::AbstractGadgetHeader,
+    simunits::Dict,
+)
+    astrip = ustrip(a)
+    haskey(simunits, prop) || return astrip
+
+    factor_func, _ = simunits[prop]
+
+    if isnothing(factor_func)
+        return astrip
+    else
+        return astrip / factor_func(h)
+    end
+end
+
+
+
 function convert_units_subfind_prop(
     val::Union{Real,AbstractArray{<:Real}},
     prop::AbstractString,
-    h,
+    h::AbstractGadgetHeader,
     units::Symbol=:full;
     verbose::Bool=false,
 )
@@ -57,176 +279,8 @@ function convert_units_subfind_prop(
 end
 
 
-# define generated functions for static linting
-function convert_units_full end
-function convert_units_physical end
-function convert_units_physical! end
-
-function simulation_units_pos end
-function simulation_units_pos! end
-function convert_units_pos end
-function convert_units_full_pos end
-function convert_units_physical_pos end
-function convert_units_physical_pos! end
-function simulation_units_vel end
-function simulation_units_vel! end
-function convert_units_vel end
-function convert_units_full_vel end
-function convert_units_physical_vel end
-function convert_units_physical_vel! end
-function simulation_units_temp end
-function simulation_units_temp! end
-function convert_units_temp end
-function convert_units_full_temp end
-function convert_units_physical_temp end
-function convert_units_physical_temp! end
-function simulation_units_mass end
-function simulation_units_mass! end
-function convert_units_mass end
-function convert_units_full_mass end
-function convert_units_physical_mass end
-function convert_units_physical_mass! end
-
-for (type, excl) in [("physical", ""), ("physical", "!"), ("full", "")]
-    quote
-        function $(Symbol("convert_units_", type, excl))(vals::AbstractArray{<:Real}, prop::Symbol, h)
-            if prop === :pos
-                $(Symbol("convert_units_", type, "_pos", excl))(vals, h)
-            elseif prop === :vel
-                $(Symbol("convert_units_", type, "_vel", excl))(vals, h)
-            elseif prop === :age
-                $(Symbol("convert_units_", type, "_age", excl))(vals, h)
-            elseif prop === :temp
-                $(Symbol("convert_units_", type, "_temp", excl))(vals, h)
-            elseif prop in [:mass, :zs, :im]
-                $(Symbol("convert_units_", type, "_mass", excl))(vals, h)
-            else
-                vals
-            end
-        end
-    end |> eval
-end
-@doc """
-    convert_units_physical(vals::AbstractArray{<:Real}, prop::Symbol, h)
-    convert_units_physical!(vals::AbstractArray{<:Real}, prop::Symbol, h)
-    convert_units_full(vals::AbstractArray{<:Real}, prop::Symbol, h)
-
-Converts simulation values to the respective physical values, depending on `prop`,
-which can take any of the following values: `:pos`, `:vel`, `:temp`, `:mass`, `:age`.
-
-Full returns values in `Unitful` quantities, whereas physical returns the physical value without unit.
-"""
-convert_units_physical,
-#convert_units_physical!,
-convert_units_full
-
-for (key, factor, unit, plural, eq, eqsim) in [
-    (
-        "pos",
-        :(1 / (h.h0 * (h.z + 1))),
-        :(u"kpc"),
-        "positions",
-        raw"x \to x/(h_0 (z+1)))",
-        raw"x \to x \times h_0 (z+1)",
-    ),
-    (
-        "vel",
-        :(1 / sqrt(h.z + 1)),
-        :(u"km/s"),
-        "velocities",
-        raw"v \to v/\sqrt{z+1})",
-        raw"v \to v \times \sqrt{z+1}",
-    ),
-    ("temp", :(1), :(u"K"), "temperatures", raw"T \to T", raw"T \to T"),
-    (
-        "mass",
-        :(1e10 / h.h0),
-        :(u"Msun"),
-        "masses",
-        raw"m \to m \times 10^{10} / h_0",
-        raw"m \to m \times h_0 / 10^{10}",
-    ),
-]
-    quote
-        function $(Symbol("convert_units_physical_", key))(val::T, h) where {T<:Real}
-            val * convert(T, $factor)
-        end
-        function $(Symbol("convert_units_full_", key))(val::Real, h)
-            $(Symbol("convert_units_physical_", key))(val, h) * $unit
-        end
-        function $(Symbol("convert_units_physical_", key))(vals::AbstractArray{T}, h) where {T<:Real}
-            vals .* convert(T, $factor)
-        end
-        function $(Symbol("convert_units_physical_", key, "!"))(vals::AbstractArray{T}, h) where {T<:Real}
-            vals .*= convert(T, $factor)
-        end
-        function $(Symbol("convert_units_full_", key))(vals::AbstractArray{T}, h) where {T<:Real}
-            vals .* (convert(T, $factor) * $unit)
-        end
-        function $(Symbol("convert_units_", key))(
-            val::Union{Real,AbstractArray{<:Real}},
-            h,
-            units::Symbol=:full,
-        )
-            if units === :physical
-                $(Symbol("convert_units_physical_", key))(val, h)
-            elseif units === :full
-                $(Symbol("convert_units_full_", key))(val, h)
-            else
-                val
-            end
-        end
-        function $(Symbol("simulation_units_", key))(val::Quantity{T}, h) where {T}
-            ustrip($unit, val) / convert(T, $factor)
-        end
-        function $(Symbol("simulation_units_", key))(val::T, h) where {T<:Real}
-            val / convert(T, $factor)
-        end
-        function $(Symbol("simulation_units_", key))(vals::AbstractArray{<:Quantity{T}}, h) where {T}
-            ustrip.($unit, vals) ./ convert(T, $factor)
-        end
-        function $(Symbol("simulation_units_", key))(vals::AbstractArray{T}, h) where {T<:Real}
-            vals ./ convert(T, $factor)
-        end
-        function $(Symbol("simulation_units_", key, "!"))(vals::AbstractArray{T}, h) where {T<:Real}
-            vals ./= convert(T, $factor)
-        end
-    end |> eval
-
-    quote
-        @doc """
-             convert_units_$($key)(val::Union{Real,AbstractArray{<:Real}}, h, units::Symbol=:full)
-             convert_units_physical_$($key)(val::Real, h)
-             convert_units_physical_$($key)(vals::AbstractArray{<:Real}, h)
-             convert_units_physical_$($key)!(vals::AbstractArray{<:Real}, h)
-             convert_units_full_$($key)(val::Real, h)
-             convert_units_full_$($key)(vals::AbstractArray{<:Real}, h)
-
-         Converts $($plural) via ``$($eq)`` according to the cosmology defined by the snapshot header.
-         `:full` returns values in `Unitful` quantities, whereas `:physical` returns the physical value
-         in $(eval($unit)). `:sim` simply returns `val`.
-         """
-        $(Symbol("convert_units_", key))#,
-        # $(Symbol("convert_units_physical_", key)),
-        # $(Symbol("convert_units_physical_", key, "!")),
-        # $(Symbol("convert_units_full_", key))
-
-        @doc """
-             simulation_units_$($key)(val::Number, h)
-             simulation_units_$($key)(vals::AbstractArray{<:Number}, h)
-             simulation_units_$($key)!(vals::AbstractArray{<:Real}, h)
-
-         Converts $($plural) via ``$($eqsim)`` according to the cosmology defined by the snapshot
-         header from physical to simulation units.
-         Unitless parameters need to be given in $(eval($unit)).
-         """
-        $(Symbol("simulation_units_", key))#,
-        # $(Symbol("simulation_units_", key, "!"))
-    end |> eval
-end
-
 @doc raw"""
-    convert_units_age(val::Union{Real,AbstractArray{<:Real}}, h, units::Symbol=:full)
+    convert_units_age(val::Union{Real,AbstractArray{<:Real}}, h::AbstractGadgetHeader, units::Symbol=:full)
     convert_units_physical_age(val::Real, h)
     convert_units_physical_age(vals::AbstractArray{<:Real}, h)
     convert_units_physical_age!(vals::AbstractArray{<:Real}, h)
@@ -237,7 +291,11 @@ Converts ages from the scale factor ``a`` via the lookback time according to the
 defined by the snapshot header.
 Full returns values in `Unitful` quantities, whereas physical returns the physical value in Gyr.
 """
-function convert_units_age(val::Union{Real,AbstractArray{<:Real}}, h, units::Symbol=:full)
+function convert_units_age(
+    val::Union{Real,AbstractArray{<:Real}},
+    h::AbstractGadgetHeader,
+    units::Symbol=:full,
+)
     if units === :physical
         convert_units_physical_age(val, h)
     elseif units === :full
